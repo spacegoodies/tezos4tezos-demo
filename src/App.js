49677c1c104e65d2c7a33c33f321520c681754bc
SpaceGoodies <rayumali@gmail.com>
2022-05-30 16:40:15 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -150,7 +150,7 @@ function App() {
     eightbidouTokenCount,
     fxhashTokenCount,
     error,
-  } = useTokensByTags([TAG, `#${TAG}`], orderColumn, platform, limit);
+  } = useTokensByTags([TAG, `${TAG}0`], orderColumn, platform, limit);
 
   if (error) {
     return <pre>{JSON.stringify(error, null, 2)}</pre>;
